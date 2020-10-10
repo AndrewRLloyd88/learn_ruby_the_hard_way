af9c5de3e1a7ffeb7f07d92541238e5be96c553d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-09 19:01:34 -0700

    complete ex3

---
+++ 03_numbers_and_math/ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens:"
+# this statement is evaluating to 30/6 + 25
+puts "Hens #{25 + 30 / 6}" #string interpolation so we will have 55 / 6ex
+
+puts "Roosters #{100 - 25 * 3 % 4}" #75 % 1.1?
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6  # does 3 go evenly into 10 no?
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 #false
+
+puts "What is 3 + 2? #{3 + 2}" #5
+puts "What is 5 - 7? #{5 - 7}" #-2
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #true
+puts "Is it greater or equal? #{5 >= -2}" #true
+puts "Is it less or equal? #{5 <= -2}" #false
