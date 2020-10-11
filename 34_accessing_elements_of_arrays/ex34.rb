5a9f5fbb9759f326e95f7d6ffbdfcf87eff49d04
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-10 21:21:10 -0700

    finish ex 34

---
+++ 34_accessing_elements_of_arrays/ex34.rb
@@ -0,0 +1,27 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals2 = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+animalat1 = animals2[1]
+puts animalat1
+
+thirdAnimal = animals2[2]
+puts thirdAnimal
+
+firstAnimal = animals2[0]
+puts firstAnimal
+
+animalat3 = animals2[2]
+puts animalat3
+
+fifthanimal = animals2[4]
+puts fifthanimal
+
+animalat2 = animals2[1]
+puts animalat2
+
+sixthanimal = animals2[5]
+puts sixthanimal
+
+animalAt4 = animals2[3]
+puts animalAt4
